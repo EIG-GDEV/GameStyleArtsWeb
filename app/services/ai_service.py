a91fc48c41e47242128527568dd074e1fcf1d18a
Enes İlyas Filiz <enesilyas21@gmail.com>
2026-09-18 00:05:34 +0300

    Model Mixtral olarak guncellendi

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -9,7 +9,7 @@ class AIService:
     def __init__(self):
         self.config = config_dict['default']
         self.api_key = os.environ.get("GROQ_API_KEY")
-        self.model = "llama-3.1-8b-instant"
+        self.model = "mixtral-8x7b-32768"
         
         
         if self.api_key:
